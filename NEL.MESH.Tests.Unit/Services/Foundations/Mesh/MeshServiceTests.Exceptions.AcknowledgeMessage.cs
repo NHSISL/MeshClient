3022a5d972af35d65df24421970e770a7586e2c4
David Hayes <david.hayes17@nhs.net>
2023-03-22 16:45:48 +0000

    ShouldThrowDependencyValidationExceptionIfServerErrorOccursOAcknowledgeMessageAsync -> FAIL

---
+++ NEL.MESH.Tests.Unit/Services/Foundations/Mesh/MeshServiceTests.Exceptions.AcknowledgeMessage.cs
@@ -0,0 +1,57 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NEL.MESH.Models.Foundations.Mesh;
+using NEL.MESH.Models.Foundations.Mesh.Exceptions;
+using Xeptions;
+using Xunit;
+
+namespace NEL.MESH.Tests.Unit.Services.Foundations.Mesh
+{
+    public partial class MeshServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationResponseMessages))]
+        public async Task ShouldThrowDependencyValidationExceptionIfServerErrorOccursOAcknowledgeMessageAsync(
+            HttpResponseMessage dependencyValidationResponseMessage)
+        {
+            // given
+            Message someMessage = CreateRandomSendMessage();
+            HttpResponseMessage response = dependencyValidationResponseMessage;
+
+            this.meshBrokerMock.Setup(broker =>
+                broker.AcknowledgeMessageAsync(It.IsAny<string>()))
+                    .ReturnsAsync(dependencyValidationResponseMessage);
+
+            var httpRequestException =
+                new HttpRequestException($"{(int)response.StatusCode} - {response.ReasonPhrase}");
+
+            var failedMeshClientException =
+                new FailedMeshClientException(httpRequestException);
+
+            var expectedMeshDependencyValidationException =
+                new MeshDependencyValidationException(failedMeshClientException.InnerException as Xeption);
+
+            // when
+            ValueTask<bool> getMessageTask =
+                this.meshService.AcknowledgeMessageAsync(someMessage.MessageId);
+
+            MeshDependencyValidationException actualMeshDependencyValidationException =
+                await Assert.ThrowsAsync<MeshDependencyValidationException>(getMessageTask.AsTask);
+
+            // then
+            actualMeshDependencyValidationException.Should().BeEquivalentTo(expectedMeshDependencyValidationException);
+
+            this.meshBrokerMock.Verify(broker =>
+                broker.AcknowledgeMessageAsync(It.IsAny<string>()),
+                    Times.Once);
+
+            this.meshBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
