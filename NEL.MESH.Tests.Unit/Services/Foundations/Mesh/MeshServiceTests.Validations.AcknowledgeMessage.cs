06dbe5254648bd76fbaa762d94e6162922b1dd78
David Hayes <david.hayes17@nhs.net>
2023-03-22 16:39:24 +0000

    ShouldThrowValidationExceptionOnAcknowledgeMessageIfMessageIdIsNullOrEmptyAsync -> FAIL

---
+++ NEL.MESH.Tests.Unit/Services/Foundations/Mesh/MeshServiceTests.Validations.AcknowledgeMessage.cs
@@ -0,0 +1,65 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NEL.MESH.Models.Foundations.Mesh;
+using NEL.MESH.Models.Foundations.Mesh.Exceptions;
+using Xunit;
+
+namespace NEL.MESH.Tests.Unit.Services.Foundations.Mesh
+{
+    public partial class MeshServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAcknowledgeMessageIfMessageIdIsNullOrEmptyAsync(string invalidText)
+        {
+            // given
+
+            Message randomMessage = CreateRandomMessage();
+            randomMessage.MessageId = invalidText;
+            Message inputMessage = randomMessage;
+            HttpResponseMessage responseMessage = CreateHttpResponseMessage(inputMessage);
+
+            this.meshBrokerMock.Setup(broker =>
+              broker.AcknowledgeMessageAsync(inputMessage.MessageId))
+                  .ReturnsAsync(responseMessage);
+
+            var InvalidMeshArgsException =
+                new InvalidMeshArgsException();
+
+            InvalidMeshArgsException.AddData(
+               key: nameof(Message.MessageId),
+               values: "Text is required");
+
+            var expectedMeshValidationException =
+                 new MeshValidationException(
+                    innerException: InvalidMeshArgsException,
+                    validationSummary: GetValidationSummary(InvalidMeshArgsException.Data));
+
+            // when
+            ValueTask<bool> getMessageTask =
+                this.meshService.AcknowledgeMessageAsync(inputMessage.MessageId);
+
+            MeshValidationException actualMeshValidationException =
+                await Assert.ThrowsAsync<MeshValidationException>(() =>
+                    getMessageTask.AsTask());
+
+            // then
+            actualMeshValidationException.Should()
+                .BeEquivalentTo(expectedMeshValidationException);
+
+            this.meshBrokerMock.Verify(broker =>
+                broker.AcknowledgeMessageAsync(inputMessage.MessageId),
+                    Times.Never);
+
+            this.meshBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
