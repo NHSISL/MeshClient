b16e3cf8b6546088a5e3c796adce2fb10c6dd01d
Christo du Toit <christo@dutoit.co.uk>
2023-03-22 15:53:29 +0000

    ShouldThrowServiceExceptionIfServiceErrorOccursOnTrackMessageAsync -> PASS

--- NEL.MESH.Tests.Unit/Services/Foundations/Mesh/MeshServiceTests.Exceptions.TrackMessage.cs
+++ NEL.MESH.Tests.Unit/Services/Foundations/Mesh/MeshServiceTests.Exceptions.TrackMessage.cs
@@ -93,5 +93,45 @@ namespace NEL.MESH.Tests.Unit.Services.Foundations.Mesh
             this.meshBrokerMock.VerifyNoOtherCalls();
         }
 
+        [Fact]
+        public async Task ShouldThrowServiceExceptionIfServiceErrorOccursOnTrackMessageAsync()
+        {
+            // given
+            Message someMessage = CreateRandomMessage();
+
+            HttpResponseMessage response = new HttpResponseMessage(System.Net.HttpStatusCode.MovedPermanently)
+            {
+                ReasonPhrase = GetRandomString()
+            };
+
+            this.meshBrokerMock.Setup(broker =>
+                broker.TrackMessageAsync(It.IsAny<string>()))
+                    .ReturnsAsync(response);
+
+            var httpRequestException =
+                new HttpRequestException($"{(int)response.StatusCode} - {response.ReasonPhrase}");
+
+            var failedMeshServiceException =
+                new FailedMeshServiceException(httpRequestException);
+
+            var expectedMeshServiceException =
+                new MeshServiceException(failedMeshServiceException as Xeption);
+
+            // when
+            ValueTask<Message> sendMessageTask =
+                this.meshService.TrackMessageAsync(someMessage.MessageId);
+
+            MeshServiceException actualMeshServiceException =
+                await Assert.ThrowsAsync<MeshServiceException>(sendMessageTask.AsTask);
+
+            // then
+            actualMeshServiceException.Should().BeEquivalentTo(expectedMeshServiceException);
+
+            this.meshBrokerMock.Verify(broker =>
+                broker.TrackMessageAsync(It.IsAny<string>()),
+                    Times.Once);
+
+            this.meshBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
