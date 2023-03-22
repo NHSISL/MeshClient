49b860b38438c8adafea9b10ad04ee8d909103dc
Christo du Toit <christo@dutoit.co.uk>
2023-03-22 12:14:17 +0000

    ShouldTrackMessageAsync -> FAIL

---
+++ NEL.MESH.Tests.Unit/Services/Foundations/Mesh/MeshServiceTests.Logic.TrackMessage.cs
@@ -0,0 +1,46 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NEL.MESH.Models.Foundations.Mesh;
+using Xunit;
+
+namespace NEL.MESH.Tests.Unit.Services.Foundations.Mesh
+{
+    public partial class MeshServiceTests
+    {
+        [Fact]
+        public async Task ShouldTrackMessageAsync()
+        {
+            // given
+            string randomString = GetRandomString();
+            string inputMessageId = randomString;
+
+            Message randomMessage = CreateRandomSendMessage();
+            Message inputMessage = randomMessage;
+            HttpResponseMessage responseMessage = CreateHttpResponseMessage(inputMessage);
+
+            this.meshBrokerMock.Setup(broker =>
+                broker.TrackMessageAsync(inputMessageId))
+                    .ReturnsAsync(responseMessage);
+
+            Message expectedMessage = GetMessageFromHttpResponseMessage(responseMessage);
+
+            // when
+            var actualMessage = await this.meshService.TrackMessageAsync(inputMessageId);
+
+            // then
+            actualMessage.Should().BeEquivalentTo(expectedMessage);
+
+            this.meshBrokerMock.Verify(broker =>
+                broker.TrackMessageAsync(inputMessageId),
+                        Times.Once);
+
+            this.meshBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
