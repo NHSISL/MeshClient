3b8a97df8b64b44983c14be080f5a1096d4a7953
Christo du Toit <christo@dutoit.co.uk>
2023-03-23 13:31:24 +0000

    CODE RUB: Excluded Integration Tests

--- NEL.MESH.Tests.Integration/Brokers/MeshBrokerTests.Logic.Handshake.cs
+++ NEL.MESH.Tests.Integration/Brokers/MeshBrokerTests.Logic.Handshake.cs
@@ -2,26 +2,21 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
-using System.Net.Http;
-using System.Threading.Tasks;
-using FluentAssertions;
-using Xunit;
-
 namespace NEL.MESH.Tests.Integration.Brokers
 {
     public partial class MeshBrokerTests
     {
-        [Fact]
-        public async Task ShouldDoHandshakeAsync()
-        {
-            // given
-            // when
-            HttpResponseMessage response = await this.meshBroker.HandshakeAsync();
-            var body = await response.Content.ReadAsStringAsync();
+        //[Fact]
+        //public async Task ShouldDoHandshakeAsync()
+        //{
+        //    // given
+        //    // when
+        //    HttpResponseMessage response = await this.meshBroker.HandshakeAsync();
+        //    var body = await response.Content.ReadAsStringAsync();
 
-            // then
-            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
-            body.Should().BeEquivalentTo($"{{\"mailboxId\":\"{this.meshConfiguration.MailboxId}\"}}");
-        }
+        //    // then
+        //    response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        //    body.Should().BeEquivalentTo($"{{\"mailboxId\":\"{this.meshConfiguration.MailboxId}\"}}");
+        //}
     }
 }
