7234816ac3db6ba36561aa4ee51bdc06ca0d677d
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-03-23 16:02:41 +0000

    ShouldThrowValidationExceptionOnSendFileIfMessageIsNullAsync -> FAIL

---
+++ NEL.MESH.Tests.Unit/Services/Foundations/Mesh/MeshServiceTests.Validations.Sendfile.cs
@@ -0,0 +1,59 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NEL.MESH.Models.Foundations.Mesh;
+using NEL.MESH.Models.Foundations.Mesh.Exceptions;
+using Xunit;
+
+namespace NEL.MESH.Tests.Unit.Services.Foundations.Mesh
+{
+    public partial class MeshServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnSendFileIfMessageIsNullAsync()
+        {
+            // given
+            Message nullMessage = null;
+
+            var nullMessageException =
+                new NullMessageException();
+
+            var expectedMeshValidationException =
+                new MeshValidationException(nullMessageException);
+
+            // when
+            ValueTask<Message> addMessageTask =
+                this.meshService.SendFileAsync(nullMessage);
+
+            MeshValidationException actualMeshValidationException =
+                await Assert.ThrowsAsync<MeshValidationException>(() =>
+                    addMessageTask.AsTask());
+
+            // then
+            actualMeshValidationException.Should()
+                .BeEquivalentTo(expectedMeshValidationException);
+
+            this.meshBrokerMock.Verify(broker =>
+                broker.SendFileAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<byte[]>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>()),
+                        Times.Never);
+
+            this.meshBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
