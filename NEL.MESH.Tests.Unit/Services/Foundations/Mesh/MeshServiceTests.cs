3f979ce5e45b4b5c4cf59fd7b8edc91d8b19a941
Christo du Toit <christo@dutoit.co.uk>
2023-03-23 10:12:12 +0000

    CODE RUB:  Fixed folder structure

---
+++ NEL.MESH.Tests.Unit/Services/Foundations/Mesh/MeshServiceTests.cs
@@ -0,0 +1,407 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Moq;
+using NEL.MESH.Brokers.Mesh;
+using NEL.MESH.Models.Foundations.Mesh;
+using NEL.MESH.Models.Foundations.Mesh.ExternalModeld;
+using NEL.MESH.Services.Mesh;
+using Newtonsoft.Json;
+using Tynamix.ObjectFiller;
+using Xunit;
+
+namespace NEL.MESH.Tests.Unit.Services.Foundations.Mesh
+{
+    public partial class MeshServiceTests
+    {
+        private readonly Mock<IMeshBroker> meshBrokerMock;
+        private readonly IMeshService meshService;
+
+        public MeshServiceTests()
+        {
+            this.meshBrokerMock = new Mock<IMeshBroker>();
+            this.meshService = new MeshService(meshBroker: this.meshBrokerMock.Object);
+        }
+
+        private static string GetRandomString() =>
+            new MnemonicString(wordCount: GetRandomNumber()).GetValue();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        public static TheoryData DependencyValidationResponseMessages()
+        {
+            var invalidValue =
+                new HttpResponseMessage((HttpStatusCode)Enum.Parse(typeof(HttpStatusCode), "400"))
+                {
+                    ReasonPhrase = "Invalid Value"
+                };
+
+            var missingValue =
+                new HttpResponseMessage((HttpStatusCode)Enum.Parse(typeof(HttpStatusCode), "400"))
+                {
+                    ReasonPhrase = "Invalid Value"
+                };
+
+            var accessDenied =
+                new HttpResponseMessage((HttpStatusCode)Enum.Parse(typeof(HttpStatusCode), "401"))
+                {
+                    ReasonPhrase = "Access Denied"
+                };
+
+            var unauthorised =
+                new HttpResponseMessage((HttpStatusCode)Enum.Parse(typeof(HttpStatusCode), "401"))
+                {
+                    ReasonPhrase = "Unauthorised"
+                };
+
+            var forbidden =
+                new HttpResponseMessage((HttpStatusCode)Enum.Parse(typeof(HttpStatusCode), "403"))
+                {
+                    ReasonPhrase = "Forbidden"
+                };
+
+            var access_Denied =
+                new HttpResponseMessage((HttpStatusCode)Enum.Parse(typeof(HttpStatusCode), "403"))
+                {
+                    ReasonPhrase = "Access Denied"
+                };
+
+            var resourceNotFound =
+                new HttpResponseMessage((HttpStatusCode)Enum.Parse(typeof(HttpStatusCode), "404"))
+                {
+                    ReasonPhrase = "Unauthorised Value"
+                };
+
+            var tooManyRequests =
+                new HttpResponseMessage((HttpStatusCode)Enum.Parse(typeof(HttpStatusCode), "429"))
+                {
+                    ReasonPhrase = "Too Many Requests"
+                };
+
+            return new TheoryData<HttpResponseMessage>
+            {
+                invalidValue,
+                missingValue,
+                accessDenied,
+                unauthorised,
+                forbidden,
+                access_Denied,
+                resourceNotFound,
+                tooManyRequests
+            };
+        }
+
+        public static TheoryData DependencyResponseMessages()
+        {
+            var internalServerError =
+                new HttpResponseMessage((HttpStatusCode)Enum.Parse(typeof(HttpStatusCode), "500"))
+                {
+                    ReasonPhrase = "Internal Server Error"
+                };
+
+            var missingValue =
+                new HttpResponseMessage((HttpStatusCode)Enum.Parse(typeof(HttpStatusCode), "501"))
+                {
+                    ReasonPhrase = "Not Implemented"
+                };
+
+            var accessDenied =
+                new HttpResponseMessage((HttpStatusCode)Enum.Parse(typeof(HttpStatusCode), "502"))
+                {
+                    ReasonPhrase = "Bad Gateway"
+                };
+
+            var unauthorised =
+                new HttpResponseMessage((HttpStatusCode)Enum.Parse(typeof(HttpStatusCode), "503"))
+                {
+                    ReasonPhrase = "Service Unavailable"
+                };
+
+            var forbidden =
+                new HttpResponseMessage((HttpStatusCode)Enum.Parse(typeof(HttpStatusCode), "504"))
+                {
+                    ReasonPhrase = "gateway Timeout"
+                };
+
+            return new TheoryData<HttpResponseMessage>
+            {
+                internalServerError,
+                missingValue,
+                accessDenied,
+                unauthorised,
+                forbidden
+            };
+        }
+
+        private static HttpResponseMessage CreateHttpResponseMessage(Message message)
+        {
+
+            string contentType = message.Headers.ContainsKey("Content-Type")
+                ? message.Headers["Content-Type"].FirstOrDefault()
+                : "text/plain";
+
+            if (string.IsNullOrEmpty(contentType))
+            {
+                contentType = "application/json";
+            }
+
+            HttpResponseMessage responseMessage = new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent("{\"messageID\": \"" + message.MessageId + "\"}", Encoding.UTF8, contentType)
+            };
+
+            responseMessage.Content.Headers.Add("Content-Encoding", string.Empty);
+            responseMessage.Content.Headers.Add("Mex-FileName", string.Empty);
+            responseMessage.Content.Headers.Add("Mex-From", string.Empty);
+            responseMessage.Content.Headers.Add("Mex-To", string.Empty);
+            responseMessage.Content.Headers.Add("Mex-WorkflowID", string.Empty);
+            responseMessage.Content.Headers.Add("Mex-Chunk-Range", string.Empty);
+            responseMessage.Content.Headers.Add("Mex-LocalID", string.Empty);
+            responseMessage.Content.Headers.Add("Mex-Subject", string.Empty);
+            responseMessage.Content.Headers.Add("Mex-Content-Checksum", string.Empty);
+            responseMessage.Content.Headers.Add("Mex-Content-Encrypted", string.Empty);
+            responseMessage.Content.Headers.Add("Mex-ClientVersion", string.Empty);
+            responseMessage.Content.Headers.Add("Mex-OSVersion", string.Empty);
+            responseMessage.Content.Headers.Add("Mex-OSArchitecture", string.Empty);
+            responseMessage.Content.Headers.Add("Mex-JavaVersion", string.Empty);
+
+            foreach (var item in message.Headers)
+            {
+                if (message.Headers.ContainsKey(item.Key))
+                {
+                    responseMessage.Content.Headers.Remove(item.Key);
+                    responseMessage.Content.Headers.Add(item.Key, item.Value);
+                }
+                else
+                {
+                    responseMessage.Content.Headers.Add(item.Key, item.Value);
+                }
+            }
+
+            return responseMessage;
+        }
+
+        private static HttpResponseMessage CreateTrackingHttpResponseMessage(TrackMessageResponse trackMessageResponse)
+        {
+            string contentType = "application/json";
+            string jsonContent = JsonConvert.SerializeObject(trackMessageResponse);
+
+            HttpResponseMessage responseMessage = new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(jsonContent, Encoding.UTF8, contentType)
+            };
+
+            return responseMessage;
+        }
+
+        private static Message GetMessageFromHttpResponseMessage(HttpResponseMessage responseMessage)
+        {
+            string responseMessageBody = responseMessage.Content.ReadAsStringAsync().Result;
+            Dictionary<string, List<string>> headers = GetHeaders(responseMessage.Content.Headers);
+
+            Message message = new Message
+            {
+                MessageId = (JsonConvert.DeserializeObject<SendMessageResponse>(responseMessageBody)).MessageId,
+                StringContent = responseMessageBody,
+                Headers = headers
+            };
+
+            return message;
+        }
+
+        private static Message GetMessageFromTrackingHttpResponseMessage(
+            string messageId,
+            HttpResponseMessage responseMessage)
+        {
+            string responseMessageBody = responseMessage.Content.ReadAsStringAsync().Result;
+            Dictionary<string, List<string>> headers = GetHeaders(responseMessage.Content.Headers);
+
+            Message message = new Message
+            {
+                MessageId = messageId,
+                TrackingInfo = JsonConvert.DeserializeObject<TrackingInfo>(responseMessageBody),
+                Headers = headers
+            };
+
+            return message;
+        }
+
+        private dynamic CreateRandomTrackingProperties()
+        {
+            return new
+            {
+                AddressType = GetRandomString(),
+                Checksum = GetRandomString(),
+                ChunkCount = GetRandomNumber(),
+                CompressFlag = GetRandomString(),
+                DownloadTimestamp = GetRandomString(),
+                DtsId = GetRandomString(),
+                EncryptedFlag = GetRandomString(),
+                ExpiryTime = GetRandomString(),
+                FileName = GetRandomString(),
+                FileSize = GetRandomNumber(),
+                IsCompressed = GetRandomString(),
+                LocalId = GetRandomString(),
+                MeshRecipientOdsCode = GetRandomString(),
+                MessageId = GetRandomString(),
+                MessageType = GetRandomString(),
+                PartnerId = GetRandomString(),
+                Recipient = GetRandomString(),
+                RecipientName = GetRandomString(),
+                RecipientOrgCode = GetRandomString(),
+                RecipientSmtp = GetRandomString(),
+                Sender = GetRandomString(),
+                SenderName = GetRandomString(),
+                SenderOdsCode = GetRandomString(),
+                SenderOrgCode = GetRandomString(),
+                SenderSmtp = GetRandomString(),
+                Status = GetRandomString(),
+                StatusSuccess = GetRandomString(),
+                UploadTimestamp = GetRandomString(),
+                Version = GetRandomString(),
+                WorkflowId = GetRandomString()
+            };
+        }
+
+        private TrackMessageResponse MapDynamicObjectToTrackMessageResponse(dynamic randomTrackingProperties)
+        {
+            return new TrackMessageResponse
+            {
+                AddressType = randomTrackingProperties.AddressType,
+                Checksum = randomTrackingProperties.Checksum,
+                ChunkCount = randomTrackingProperties.ChunkCount,
+                CompressFlag = randomTrackingProperties.CompressFlag,
+                DownloadTimestamp = randomTrackingProperties.DownloadTimestamp,
+                DtsId = randomTrackingProperties.DtsId,
+                EncryptedFlag = randomTrackingProperties.EncryptedFlag,
+                ExpiryTime = randomTrackingProperties.ExpiryTime,
+                FileName = randomTrackingProperties.FileName,
+                FileSize = randomTrackingProperties.FileSize,
+                IsCompressed = randomTrackingProperties.IsCompressed,
+                LocalId = randomTrackingProperties.LocalId,
+                MeshRecipientOdsCode = randomTrackingProperties.MeshRecipientOdsCode,
+                MessageId = randomTrackingProperties.MessageId,
+                MessageType = randomTrackingProperties.MessageType,
+                PartnerId = randomTrackingProperties.PartnerId,
+                Recipient = randomTrackingProperties.Recipient,
+                RecipientName = randomTrackingProperties.RecipientName,
+                RecipientOrgCode = randomTrackingProperties.RecipientOrgCode,
+                RecipientSmtp = randomTrackingProperties.RecipientSmtp,
+                Sender = randomTrackingProperties.Sender,
+                SenderName = randomTrackingProperties.SenderName,
+                SenderOdsCode = randomTrackingProperties.SenderOdsCode,
+                SenderOrgCode = randomTrackingProperties.SenderOrgCode,
+                SenderSmtp = randomTrackingProperties.SenderSmtp,
+                Status = randomTrackingProperties.Status,
+                StatusSuccess = randomTrackingProperties.StatusSuccess,
+                UploadTimestamp = randomTrackingProperties.UploadTimestamp,
+                Version = randomTrackingProperties.Version,
+                WorkflowId = randomTrackingProperties.WorkflowId
+            };
+        }
+
+        private TrackingInfo MapDynamicObjectToTrackingInfo(dynamic randomTrackingProperties)
+        {
+            return new TrackingInfo
+            {
+                AddressType = randomTrackingProperties.AddressType,
+                Checksum = randomTrackingProperties.Checksum,
+                ChunkCount = randomTrackingProperties.ChunkCount,
+                CompressFlag = randomTrackingProperties.CompressFlag,
+                DownloadTimestamp = randomTrackingProperties.DownloadTimestamp,
+                DtsId = randomTrackingProperties.DtsId,
+                EncryptedFlag = randomTrackingProperties.EncryptedFlag,
+                ExpiryTime = randomTrackingProperties.ExpiryTime,
+                FileName = randomTrackingProperties.FileName,
+                FileSize = randomTrackingProperties.FileSize,
+                IsCompressed = randomTrackingProperties.IsCompressed,
+                LocalId = randomTrackingProperties.LocalId,
+                MeshRecipientOdsCode = randomTrackingProperties.MeshRecipientOdsCode,
+                MessageId = randomTrackingProperties.MessageId,
+                MessageType = randomTrackingProperties.MessageType,
+                PartnerId = randomTrackingProperties.PartnerId,
+                Recipient = randomTrackingProperties.Recipient,
+                RecipientName = randomTrackingProperties.RecipientName,
+                RecipientOrgCode = randomTrackingProperties.RecipientOrgCode,
+                RecipientSmtp = randomTrackingProperties.RecipientSmtp,
+                Sender = randomTrackingProperties.Sender,
+                SenderName = randomTrackingProperties.SenderName,
+                SenderOdsCode = randomTrackingProperties.SenderOdsCode,
+                SenderOrgCode = randomTrackingProperties.SenderOrgCode,
+                SenderSmtp = randomTrackingProperties.SenderSmtp,
+                Status = randomTrackingProperties.Status,
+                StatusSuccess = randomTrackingProperties.StatusSuccess,
+                UploadTimestamp = randomTrackingProperties.UploadTimestamp,
+                Version = randomTrackingProperties.Version,
+                WorkflowId = randomTrackingProperties.WorkflowId
+            };
+        }
+
+        private static Dictionary<string, List<string>> GetHeaders(HttpContentHeaders headers)
+        {
+            var dictionary = new Dictionary<string, List<string>>();
+
+            foreach (var header in headers)
+            {
+                dictionary.Add(header.Key, header.Value.ToList());
+            }
+
+            return dictionary;
+        }
+
+        private static Message CreateRandomSendMessage()
+        {
+            var message = CreateMessageFiller().Create();
+            message.Headers.Add("Content-Type", new List<string> { "text/plain" });
+            message.Headers.Add("Mex-LocalID", new List<string> { GetRandomString() });
+            message.Headers.Add("Mex-Subject", new List<string> { GetRandomString() });
+            message.Headers.Add("Mex-Content-Encrypted", new List<string> { "encrypted" });
+            message.Headers.Add("Mex-From", new List<string> { GetRandomString() });
+            message.Headers.Add("Mex-To", new List<string> { GetRandomString() });
+            message.Headers.Add("Mex-WorkflowID", new List<string> { GetRandomString() });
+            message.Headers.Add("Mex-FileName", new List<string> { GetRandomString() });
+
+            return message;
+        }
+
+        private static Message CreateRandomMessage() =>
+            CreateMessageFiller().Create();
+
+        private static Filler<Message> CreateMessageFiller()
+        {
+            var filler = new Filler<Message>();
+            filler.Setup().OnProperty(message => message.Headers)
+                .Use(new Dictionary<string, List<string>>());
+
+            return filler;
+        }
+
+        private string GetValidationSummary(IDictionary data)
+        {
+            StringBuilder validationSummary = new StringBuilder();
+
+            foreach (DictionaryEntry entry in data)
+            {
+                string errorSummary = ((List<string>)entry.Value)
+                    .Select((string value) => value)
+                    .Aggregate((string current, string next) => current + ", " + next);
+
+                validationSummary.Append($"{entry.Key} => {errorSummary};  ");
+            }
+
+            return validationSummary.ToString();
+        }
+    }
+}
