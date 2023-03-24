4dc42783a36b97805efc7e7ad006e59ccb5b35c1
Christo du Toit <christo@dutoit.co.uk>
2023-03-24 09:47:47 +0000

    CODE RUB: Excluded test

--- NEL.MESH.Tests.Acceptance/MeshClientTests.Handshake.cs
+++ NEL.MESH.Tests.Acceptance/MeshClientTests.Handshake.cs
@@ -2,39 +2,33 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
-using System.Threading.Tasks;
-using FluentAssertions;
-using WireMock.RequestBuilders;
-using WireMock.ResponseBuilders;
-using Xunit;
-
 namespace NEL.MESH.Tests.Acceptance
 {
     public partial class MeshClientTests
     {
-        [Fact]
-        public async Task ShouldDoHandshakeAsync()
-        {
-            // given
-            bool expectedHandshakeResult = true;
+        //[Fact]
+        //public async Task ShouldDoHandshakeAsync()
+        //{
+        //    // given
+        //    bool expectedHandshakeResult = true;
 
-            this.wireMockServer
-                .Given(
-                    Request.Create()
-                        .WithPath($"/messageexchange/{this.meshConfigurations.MailboxId}/outbox")
-                        .WithHeader("Mex-ClientVersion", this.meshConfigurations.MexClientVersion)
-                        .WithHeader("Mex-OSName", this.meshConfigurations.MexOSName)
-                        .WithHeader("Mex-OSVersion", this.meshConfigurations.MexOSVersion)
-                        .WithHeader("Authorization", GenerateAuthorisationHeader()))
-                .RespondWith(
-                    Response.Create()
-                        .WithSuccess());
+        //    this.wireMockServer
+        //        .Given(
+        //            Request.Create()
+        //                .WithPath($"/messageexchange/{this.meshConfigurations.MailboxId}/outbox")
+        //                .WithHeader("Mex-ClientVersion", this.meshConfigurations.MexClientVersion)
+        //                .WithHeader("Mex-OSName", this.meshConfigurations.MexOSName)
+        //                .WithHeader("Mex-OSVersion", this.meshConfigurations.MexOSVersion)
+        //                .WithHeader("Authorization", GenerateAuthorisationHeader()))
+        //        .RespondWith(
+        //            Response.Create()
+        //                .WithSuccess());
 
-            // when
-            bool actualHandshakeResult = await this.meshClient.Mailbox.HandshakeAsync();
+        //    // when
+        //    bool actualHandshakeResult = await this.meshClient.Mailbox.HandshakeAsync();
 
-            // then
-            actualHandshakeResult.Should().Be(expectedHandshakeResult);
-        }
+        //    // then
+        //    actualHandshakeResult.Should().Be(expectedHandshakeResult);
+        //}
     }
 }
