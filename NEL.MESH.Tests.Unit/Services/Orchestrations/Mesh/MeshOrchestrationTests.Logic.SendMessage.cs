eb12bb9b512062e8eb92126c5fcbd3c951c5ce47
Christo du Toit <christo@dutoit.co.uk>
2023-03-27 14:48:33 +0100

    ShouldSendMessageAsync -> FAIL

---
+++ NEL.MESH.Tests.Unit/Services/Orchestrations/Mesh/MeshOrchestrationTests.Logic.SendMessage.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using NEL.MESH.Models.Foundations.Mesh;
+using Xunit;
+
+namespace NEL.MESH.Tests.Unit.Services.Orchestrations.Mesh
+{
+    public partial class MeshOrchestrationTests
+    {
+        [Fact]
+        public async Task ShouldSendMessageAsync()
+        {
+            // given
+            string randomToken = GetRandomString();
+            string randomMailboxId = GetRandomString();
+            string randomPassword = GetRandomString();
+            string randomKey = GetRandomString();
+            Message randomMessage = CreateRandomSendMessage();
+            Message inputMessage = randomMessage;
+            Message outputMessage = inputMessage.DeepClone();
+            Message expectedMessage = outputMessage;
+
+            this.tokenServiceMock.Setup(service =>
+                service.GenerateTokenAsync(randomMailboxId, randomPassword, randomKey))
+                    .ReturnsAsync(randomToken);
+
+            this.meshServiceMock.Setup(service =>
+                service.SendMessageAsync(inputMessage, randomToken))
+                    .ReturnsAsync(outputMessage);
+
+            // when
+            Message actualMessage = await this.meshOrchestrationService
+                .SendMessageAsync(message: inputMessage);
+
+            // then
+            actualMessage.Should().BeEquivalentTo(expectedMessage);
+
+            this.tokenServiceMock.Verify(service =>
+                service.GenerateTokenAsync(randomMailboxId, randomPassword, randomKey),
+                    Times.Once);
+
+            this.meshServiceMock.Verify(service =>
+                service.SendMessageAsync(inputMessage, randomToken),
+                    Times.Once);
+
+            this.meshServiceMock.VerifyNoOtherCalls();
+            this.tokenServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
