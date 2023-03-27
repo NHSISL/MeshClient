317b984f245ae4cec68047b10bfcdc2ee8402944
Christo du Toit <christo@dutoit.co.uk>
2023-03-27 11:29:07 +0100

    ShouldThrowServiceExceptionIfServiceErrorOccursOnHandshakeAsync -> PASS

--- NEL.MESH/Services/Foundations/Tokens/TokenService.Exceptions.cs
+++ NEL.MESH/Services/Foundations/Tokens/TokenService.Exceptions.cs
@@ -2,6 +2,7 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using NEL.MESH.Models.Foundations.Token.Exceptions;
 using Xeptions;
@@ -22,16 +23,31 @@ namespace NEL.MESH.Services.Foundations.Tokens
             {
                 throw CreateAndLogValidationException(invalidArgumentTokenException);
             }
+            catch (Exception exception)
+            {
+                var failedTokenServiceException =
+                    new FailedTokenServiceException(exception);
+
+                throw CreateAndLogServiceException(failedTokenServiceException);
+            }
         }
 
         private TokenValidationException CreateAndLogValidationException(Xeption exception)
         {
             string validationSummary = GetValidationSummary(exception.Data);
 
-            var meshValidationException =
+            var tokenValidationException =
                 new TokenValidationException(exception, validationSummary);
 
-            return meshValidationException;
+            return tokenValidationException;
+        }
+
+        private TokenServiceException CreateAndLogServiceException(Xeption exception)
+        {
+            var tokenServiceException = new
+                TokenServiceException(exception);
+
+            return tokenServiceException;
         }
     }
 }
