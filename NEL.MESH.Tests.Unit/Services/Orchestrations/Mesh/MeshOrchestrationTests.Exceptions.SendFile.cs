a90a3bd2d5406440f677180eb57b111ce61c42a7
Christo du Toit <christo@dutoit.co.uk>
2023-03-29 10:23:13 +0100

    ShouldThrowDependencyValidationExceptionOnSendFileIfDependencyValidationErrorOccursAsync -> PASS
    ShouldThrowDependencyExceptionOnSendFileIfDependencyErrorOccursAsync -> PASS
    
    ShouldThrowServiceExceptionOnSendFileIfServiceErrorOccursAsync -> PASS

---
+++ NEL.MESH.Tests.Unit/Services/Orchestrations/Mesh/MeshOrchestrationTests.Exceptions.SendFile.cs
@@ -0,0 +1,120 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NEL.MESH.Models.Foundations.Mesh;
+using NEL.MESH.Models.Orchestrations.Mesh.Exceptions;
+using Xeptions;
+using Xunit;
+
+namespace NEL.MESH.Tests.Unit.Services.Orchestrations.Mesh
+{
+    public partial class MeshOrchestrationTests
+    {
+        [Theory]
+        [MemberData(nameof(MeshDependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnSendFileIfDependencyValidationErrorOccursAsync(
+            Xeption dependancyValidationException)
+        {
+            // given
+            Message someMessage = CreateRandomSendMessage();
+            var expectedMeshOrchestrationDependencyValidationException =
+            new MeshOrchestrationDependencyValidationException(
+                dependancyValidationException.InnerException as Xeption);
+
+            this.tokenServiceMock.Setup(service =>
+                service.GenerateTokenAsync())
+                    .ThrowsAsync(dependancyValidationException);
+
+            // when
+            ValueTask<Message> sendMessageTask = this.meshOrchestrationService.SendFileAsync(someMessage);
+
+            MeshOrchestrationDependencyValidationException actualMeshOrchestrationDependencyValidationException =
+                await Assert.ThrowsAsync<MeshOrchestrationDependencyValidationException>(sendMessageTask.AsTask);
+
+            // then
+            actualMeshOrchestrationDependencyValidationException.Should()
+                .BeEquivalentTo(expectedMeshOrchestrationDependencyValidationException);
+
+            this.tokenServiceMock.Verify(service =>
+                service.GenerateTokenAsync(),
+                    Times.Once);
+
+            this.meshServiceMock.VerifyNoOtherCalls();
+            this.tokenServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(MeshDependencyExceptions))]
+        public async Task ShouldThrowDependencyExceptionOnSendFileIfDependencyErrorOccursAsync(
+            Xeption dependancyException)
+        {
+            // given
+            Message someMessage = CreateRandomSendMessage();
+            var expectedMeshOrchestrationDependencyException =
+            new MeshOrchestrationDependencyException(
+                dependancyException.InnerException as Xeption);
+
+            this.tokenServiceMock.Setup(service =>
+                service.GenerateTokenAsync())
+                    .ThrowsAsync(dependancyException);
+
+            // when
+            ValueTask<Message> sendMessageTask = this.meshOrchestrationService.SendFileAsync(someMessage);
+
+            MeshOrchestrationDependencyException actualMeshOrchestrationDependencyException =
+                await Assert.ThrowsAsync<MeshOrchestrationDependencyException>(sendMessageTask.AsTask);
+
+            // then
+            actualMeshOrchestrationDependencyException.Should()
+                .BeEquivalentTo(expectedMeshOrchestrationDependencyException);
+
+            this.tokenServiceMock.Verify(service =>
+                service.GenerateTokenAsync(),
+                    Times.Once);
+
+            this.meshServiceMock.VerifyNoOtherCalls();
+            this.tokenServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnSendFileIfServiceErrorOccursAsync()
+        {
+            // given
+            Message someMessage = CreateRandomSendMessage();
+            string someErrorMessage = GetRandomString();
+            var serviceException = new Exception(someErrorMessage);
+
+            var failedMeshOrchestrationServiceException =
+                new FailedMeshOrchestrationServiceException(serviceException);
+
+            var expectedMeshOrchestrationServiceException =
+            new MeshOrchestrationServiceException(failedMeshOrchestrationServiceException);
+
+            this.tokenServiceMock.Setup(service =>
+                service.GenerateTokenAsync())
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<Message> sendMessageTask = this.meshOrchestrationService.SendFileAsync(someMessage);
+
+            MeshOrchestrationServiceException actualMeshOrchestrationServiceException =
+                await Assert.ThrowsAsync<MeshOrchestrationServiceException>(sendMessageTask.AsTask);
+
+            // then
+            actualMeshOrchestrationServiceException.Should()
+                .BeEquivalentTo(expectedMeshOrchestrationServiceException);
+
+            this.tokenServiceMock.Verify(service =>
+                service.GenerateTokenAsync(),
+                    Times.Once);
+
+            this.meshServiceMock.VerifyNoOtherCalls();
+            this.tokenServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
