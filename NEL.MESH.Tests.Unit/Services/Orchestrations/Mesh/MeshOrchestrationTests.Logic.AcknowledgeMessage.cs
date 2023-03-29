95be20be5d59a5f71e07f7781092f7594a39435f
David Hayes <david.hayes17@nhs.net>
2023-03-29 14:31:38 +0100

    ShouldAcknowledgeMessageAsync -> FAIL

---
+++ NEL.MESH.Tests.Unit/Services/Orchestrations/Mesh/MeshOrchestrationTests.Logic.AcknowledgeMessage.cs
@@ -0,0 +1,44 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace NEL.MESH.Tests.Unit.Services.Orchestrations.Mesh
+{
+    public partial class MeshOrchestrationTests
+    {
+        [Fact]
+        public async Task ShouldAcknowledgeMessageAsync()
+        {
+            // given
+            string randomToken = GetRandomString();
+
+            bool outputValidationResult = true;
+            bool expectedValidationResult = outputValidationResult;
+
+            string randomMessageId = GetRandomString();
+            string inputMessageId = randomMessageId;
+
+            this.meshServiceMock.Setup(service =>
+                service.AcknowledgeMessageAsync(inputMessageId, randomToken))
+                    .ReturnsAsync(expectedValidationResult);
+
+            // when
+            bool actualResult = await this.meshOrchestrationService.AcknowledgeMessageAsync(inputMessageId);
+
+            // then
+            actualResult.Should().BeTrue();
+
+            this.meshServiceMock.Verify(service =>
+                service.AcknowledgeMessageAsync(inputMessageId, randomToken),
+                    Times.Once);
+
+            this.meshServiceMock.VerifyNoOtherCalls();
+            this.tokenServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
