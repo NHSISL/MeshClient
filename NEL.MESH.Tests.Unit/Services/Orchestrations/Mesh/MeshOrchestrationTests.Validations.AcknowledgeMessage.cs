92ecfe982329bd64d54af55cc8d318ffcf3cd894
David Hayes <david.hayes17@nhs.net>
2023-03-29 14:53:32 +0100

    ShouldThrowValidationExceptionOnAknowledgeMessageIfMessageIsNullAndLogItAsync -> FAIL

---
+++ NEL.MESH.Tests.Unit/Services/Orchestrations/Mesh/MeshOrchestrationTests.Validations.AcknowledgeMessage.cs
@@ -0,0 +1,60 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NEL.MESH.Models.Foundations.Mesh;
+using NEL.MESH.Models.Foundations.Token.Exceptions;
+using NEL.MESH.Models.Orchestrations.Mesh.Exceptions;
+using Xunit;
+
+namespace NEL.MESH.Tests.Unit.Services.Orchestrations.Mesh
+{
+    public partial class MeshOrchestrationTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAknowledgeMessageIfMessageIsNullAndLogItAsync(string invalidText)
+        {
+            // given
+            string invalidMessageId = invalidText;
+            string randomToken = GetRandomString();
+            Message randomMessage = CreateRandomSendMessage();
+
+            var invalidMeshOrchestrationArgsException =
+                  new InvalidMeshOrchestrationArgsException();
+
+            invalidMeshOrchestrationArgsException.AddData(
+                key: nameof(Message.MessageId),
+                values: "Text is required");
+
+            var expectedMeshOrchestrationValidationException =
+                new MeshOrchestrationValidationException(
+                    innerException: invalidMeshOrchestrationArgsException,
+                    validationSummary: GetValidationSummary(invalidMeshOrchestrationArgsException.Data));
+
+            // when
+            ValueTask<bool> messageTask = this.meshOrchestrationService
+                .AcknowledgeMessageAsync(messageId: invalidMessageId);
+
+            MeshOrchestrationValidationException actualMeshOrchestrationValidationException =
+                await Assert.ThrowsAsync<MeshOrchestrationValidationException>(messageTask.AsTask);
+
+            // then
+            actualMeshOrchestrationValidationException.Should()
+                .BeEquivalentTo(expectedMeshOrchestrationValidationException);
+
+            this.tokenServiceMock.Verify(service =>
+                service.GenerateTokenAsync(),
+                    Times.Never);
+
+            this.meshServiceMock.VerifyNoOtherCalls();
+            this.tokenServiceMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
