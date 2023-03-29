2b411f800338b8d3949c48bcc05e11612a7d132b
David Hayes <david.hayes17@nhs.net>
2023-03-29 15:03:39 +0100

    ShouldThrowDependencyValidationExceptionOnAcknowledgeMessageIfDependencyValidationErrorOccursAsync -> PASS

---
+++ NEL.MESH.Tests.Unit/Services/Orchestrations/Mesh/MeshOrchestrationTests.Exceptions.AcknowledgeMessage.cs
@@ -0,0 +1,50 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NEL.MESH.Models.Orchestrations.Mesh.Exceptions;
+using Xeptions;
+using Xunit;
+
+namespace NEL.MESH.Tests.Unit.Services.Orchestrations.Mesh
+{
+    public partial class MeshOrchestrationTests
+    {
+        [Theory]
+        [MemberData(nameof(MeshDependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnAcknowledgeMessageIfDependencyValidationErrorOccursAsync(
+            Xeption dependancyValidationException)
+        {
+            // given
+            string someMessageId = GetRandomString();
+            var expectedMeshOrchestrationDependencyValidationException =
+            new MeshOrchestrationDependencyValidationException(
+                dependancyValidationException.InnerException as Xeption);
+
+            this.tokenServiceMock.Setup(service =>
+                service.GenerateTokenAsync())
+                    .ThrowsAsync(dependancyValidationException);
+
+            // when
+            ValueTask<bool> acknowledgeMessageTask = this.meshOrchestrationService.AcknowledgeMessageAsync(someMessageId);
+
+            MeshOrchestrationDependencyValidationException actualMeshOrchestrationDependencyValidationException =
+                await Assert.ThrowsAsync<MeshOrchestrationDependencyValidationException>(acknowledgeMessageTask.AsTask);
+
+            // then
+            actualMeshOrchestrationDependencyValidationException.Should()
+                .BeEquivalentTo(expectedMeshOrchestrationDependencyValidationException);
+
+            this.tokenServiceMock.Verify(service =>
+                service.GenerateTokenAsync(),
+                    Times.Once);
+
+            this.meshServiceMock.VerifyNoOtherCalls();
+            this.tokenServiceMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
