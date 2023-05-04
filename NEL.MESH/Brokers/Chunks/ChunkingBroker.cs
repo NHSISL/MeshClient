79a5f531631812cc2576855f62dc02524498e238
Christo du Toit <christo@dutoit.co.uk>
2023-05-04 11:55:18 +0100

    BROKERS: Chunking

---
+++ NEL.MESH/Brokers/Chunks/ChunkingBroker.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using NEL.MESH.Models.Configurations;
+
+namespace NEL.MESH.Brokers.Chunks
+{
+    internal class ChunkingBroker : IChunkingBroker
+    {
+        private readonly MeshConfiguration meshConfiguration;
+
+        public ChunkingBroker(MeshConfiguration meshConfiguration)
+        {
+            this.meshConfiguration = meshConfiguration;
+        }
+
+        public float ChunkSizeInMegabytes => this.meshConfiguration.ChunkSizeInMegabytes;
+    }
+}
---
+++ NEL.MESH/Brokers/Chunks/IChunkingBroker.cs
@@ -0,0 +1,11 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+namespace NEL.MESH.Brokers.Chunks
+{
+    internal interface IChunkingBroker
+    {
+        float ChunkSizeInMegabytes { get; }
+    }
+}
