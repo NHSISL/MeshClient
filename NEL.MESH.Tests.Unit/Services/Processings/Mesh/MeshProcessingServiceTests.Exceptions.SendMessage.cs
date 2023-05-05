a54315210f455d327e05f80f24593d8654530d2f
Christo du Toit <christo@dutoit.co.uk>
2023-05-05 16:41:41 +0100

    ShouldThrowDependencyExceptionOnSendMessageIfDependencyErrorOccurs -> FAIL

--- NEL.MESH.Tests.Unit/Services/Processings/Mesh/MeshProcessingServiceTests.Exceptions.SendMessage.cs
+++ NEL.MESH.Tests.Unit/Services/Processings/Mesh/MeshProcessingServiceTests.Exceptions.SendMessage.cs
@@ -5,6 +5,7 @@
 using System.Threading.Tasks;
 using FluentAssertions;
 using Moq;
+using NEL.MESH.Models.Clients.Mesh.Exceptions;
 using NEL.MESH.Models.Foundations.Mesh;
 using NEL.MESH.Models.Processings.Mesh;
 using Xeptions;
@@ -32,15 +33,50 @@ namespace NEL.MESH.Tests.Unit.Services.Processings.Mesh
                     .ThrowsAsync(dependencyValidationException);
 
             // when
-            ValueTask<Message> HandshakeTask =
+            ValueTask<Message> sendMessageTask =
                 this.meshProcessingService.SendMessageAsync(someMessage, someAuthorizationToken);
 
             MeshProcessingDependencyValidationException actualMeshProcessingDependencyValidationException =
-                await Assert.ThrowsAsync<MeshProcessingDependencyValidationException>(HandshakeTask.AsTask);
+                await Assert.ThrowsAsync<MeshProcessingDependencyValidationException>(sendMessageTask.AsTask);
 
             // then
             actualMeshProcessingDependencyValidationException.Should()
                 .BeEquivalentTo(expectedMeshProcessingDependencyValidationException);
+
+            this.meshServiceMock.Verify(service =>
+                service.SendMessageAsync(It.IsAny<Message>(), It.IsAny<string>()),
+                    Times.Once);
+
+            this.meshServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public async Task ShouldThrowDependencyExceptionOnSendMessageIfDependencyErrorOccurs(
+            Xeption dependencyException)
+        {
+            // given
+            string someAuthorizationToken = GetRandomString();
+            Message someMessage = CreateRandomMessage();
+
+            var expectedMeshProcessingDependencyException =
+                new MeshProcessingDependencyException(
+                    dependencyException.InnerException as Xeption);
+
+            this.meshServiceMock.Setup(service =>
+                service.SendMessageAsync(It.IsAny<Message>(), It.IsAny<string>()))
+                    .ThrowsAsync(dependencyException);
+
+            // when
+            ValueTask<Message> sendMessageTask =
+                this.meshProcessingService.SendMessageAsync(someMessage, someAuthorizationToken);
+
+            MeshProcessingDependencyException actualMeshProcessingDependencyException =
+                await Assert.ThrowsAsync<MeshProcessingDependencyException>(sendMessageTask.AsTask);
+
+            // then
+            actualMeshProcessingDependencyException.Should()
+                .BeEquivalentTo(expectedMeshProcessingDependencyException);
 
             this.meshServiceMock.Verify(service =>
                 service.SendMessageAsync(It.IsAny<Message>(), It.IsAny<string>()),
