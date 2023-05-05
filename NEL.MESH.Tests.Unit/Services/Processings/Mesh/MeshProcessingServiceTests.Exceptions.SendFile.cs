62d0a0efd1d9d574c4f9339e527fccdefdda4a3d
Christo du Toit <christo@dutoit.co.uk>
2023-05-05 21:06:13 +0100

    ShouldThrowDependencyValidationExceptionOnSendFileIfDependencyValidationErrorOccurs -> PASS

---
+++ NEL.MESH.Tests.Unit/Services/Processings/Mesh/MeshProcessingServiceTests.Exceptions.SendFile.cs
@@ -0,0 +1,52 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NEL.MESH.Models.Foundations.Mesh;
+using NEL.MESH.Models.Processings.Mesh;
+using Xeptions;
+using Xunit;
+
+namespace NEL.MESH.Tests.Unit.Services.Processings.Mesh
+{
+    public partial class MeshProcessingServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnSendFileIfDependencyValidationErrorOccurs(
+            Xeption dependencyValidationException)
+        {
+            // given
+            string someAuthorizationToken = GetRandomString();
+            Message someMessage = CreateRandomMessage();
+
+            var expectedMeshProcessingDependencyValidationException =
+                new MeshProcessingDependencyValidationException(
+                    dependencyValidationException.InnerException as Xeption);
+
+            this.meshServiceMock.Setup(service =>
+                service.SendFileAsync(It.IsAny<Message>(), It.IsAny<string>()))
+                    .ThrowsAsync(dependencyValidationException);
+
+            // when
+            ValueTask<Message> sendMessageTask =
+                this.meshProcessingService.SendFileAsync(someMessage, someAuthorizationToken);
+
+            MeshProcessingDependencyValidationException actualMeshProcessingDependencyValidationException =
+                await Assert.ThrowsAsync<MeshProcessingDependencyValidationException>(sendMessageTask.AsTask);
+
+            // then
+            actualMeshProcessingDependencyValidationException.Should()
+                .BeEquivalentTo(expectedMeshProcessingDependencyValidationException);
+
+            this.meshServiceMock.Verify(service =>
+                service.SendFileAsync(It.IsAny<Message>(), It.IsAny<string>()),
+                    Times.Once);
+
+            this.meshServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
