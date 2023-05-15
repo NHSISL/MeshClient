4901b323b87c33a42886575c0fbb70de9480ffdb
Christo du Toit <christo@dutoit.co.uk>
2023-05-15 16:47:12 +0100

    INFRASTRUCTURE: Tagging Releases

---
+++ NEL.MESH.Infrastructure/Services/ScriptGenerationService.cs
@@ -0,0 +1,94 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+namespace NEL.MESH.Infrastructure.Services
+{
+    internal class ScriptGenerationService
+    {
+        private readonly ADotNetClient adotNetClient;
+
+        public ScriptGenerationService() =>
+            this.adotNetClient = new ADotNetClient();
+
+        public void GenerateBuildScript()
+        {
+            var githubPipeline = new GithubPipeline
+            {
+                Name = ".Net",
+
+                OnEvents = new Events
+                {
+                    Push = new PushEvent
+                    {
+                        Branches = new string[] { "main" }
+                    },
+
+                    PullRequest = new PullRequestEvent
+                    {
+                        Branches = new string[] { "main" }
+                    }
+                },
+
+                Jobs = new Jobs
+                {
+                    Build = new BuildJob
+                    {
+                        RunsOn = BuildMachines.Windows2019,
+
+                        Steps = new List<GithubTask>
+                    {
+                        new CheckoutTaskV2
+                        {
+                            Name = "Check Out"
+                        },
+
+                        new SetupDotNetTaskV1
+                        {
+                            Name = "Setup Dot Net Version",
+
+                            TargetDotNetVersion = new TargetDotNetVersion
+                            {
+                                DotNetVersion = "7.0.100",
+                                IncludePrerelease = false
+                            }
+                        },
+
+                        new RestoreTask
+                        {
+                            Name = "Restore"
+                        },
+
+                        new DotNetBuildTask
+                        {
+                            Name = "Build"
+                        },
+
+                        new TestTask
+                        {
+                            Name = "Test"
+                        }
+                    }
+                    }
+                }
+            };
+
+            string buildScriptPath = "../../../../.github/workflows/build.yml";
+            string directoryPath = Path.GetDirectoryName(buildScriptPath);
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            this.adotNetClient.SerializeAndWriteToFile(
+                githubPipeline,
+                path: buildScriptPath);
+        }
+    }
+}
