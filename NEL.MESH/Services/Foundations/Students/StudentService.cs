574e868200e629e1afeaa785ada56eaf529d4ea1
Christo du Toit <christo@dutoit.co.uk>
2023-05-24 09:59:18 +0100

    ShouldThrowNotFoundExceptionOnRemoveIfStudentIsNotFoundAndLogItAsync -> PASS

--- NEL.MESH/Services/Foundations/Students/StudentService.cs
+++ NEL.MESH/Services/Foundations/Students/StudentService.cs
@@ -70,6 +70,8 @@ namespace NEL.MESH.Services.Foundations.Students
                 Student maybeStudent = await this.storageBroker
                     .SelectStudentByIdAsync(studentId);
 
+                ValidateStorageStudent(maybeStudent, studentId);
+
                 return await this.storageBroker.DeleteStudentAsync(maybeStudent);
             });
     }
