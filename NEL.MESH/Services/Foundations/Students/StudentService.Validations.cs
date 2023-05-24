77231f11110eac34c2a1fa91a2b88336042cb1e6
Christo du Toit <christo@dutoit.co.uk>
2023-05-24 09:58:20 +0100

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> PASS

--- NEL.MESH/Services/Foundations/Students/StudentService.Validations.cs
+++ NEL.MESH/Services/Foundations/Students/StudentService.Validations.cs
@@ -35,6 +35,9 @@ namespace NEL.MESH.Services.Foundations.Students
                 (Rule: IsNotRecent(student.CreatedDate), Parameter: nameof(Student.CreatedDate)));
         }
 
+        public void ValidateStudentId(Guid studentId) =>
+            Validate((Rule: IsInvalid(studentId), Parameter: nameof(Student.Id)));
+
         private static void ValidateStudentIsNotNull(Student student)
         {
             if (student is null)
