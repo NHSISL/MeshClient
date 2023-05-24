29eadddb1e5509fe6f2e9ea49b5ab91c17e33f43
Christo du Toit <christo@dutoit.co.uk>
2023-05-24 09:58:41 +0100

    ShouldThrowValidationExceptionOnModifyIfUpdatedDateIsSameAsCreatedDateAndLogItAsync -> PASS

--- NEL.MESH.Tests.Unit/Services/Foundations/Students/StudentServiceTests.Validations.Modify.cs
+++ NEL.MESH.Tests.Unit/Services/Foundations/Students/StudentServiceTests.Validations.Modify.cs
@@ -84,7 +84,11 @@ namespace NEL.MESH.Tests.Unit.Services.Foundations.Students
 
             invalidStudentException.AddData(
                 key: nameof(Student.UpdatedDate),
-                values: "Date is required");
+                values:
+                new[] {
+                    "Date is required",
+                    $"Date is the same as {nameof(Student.CreatedDate)}"
+                });
 
             invalidStudentException.AddData(
                 key: nameof(Student.UpdatedByUserId),
