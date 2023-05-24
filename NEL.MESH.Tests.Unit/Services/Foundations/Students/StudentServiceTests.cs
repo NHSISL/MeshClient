b4ebfb10354d1f3573c589183b4a1f4ee5f187da
Christo du Toit <christo@dutoit.co.uk>
2023-05-24 09:57:48 +0100

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> FAIL

--- NEL.MESH.Tests.Unit/Services/Foundations/Students/StudentServiceTests.cs
+++ NEL.MESH.Tests.Unit/Services/Foundations/Students/StudentServiceTests.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Linq.Expressions;
+using System.Runtime.Serialization;
+using Microsoft.Data.SqlClient;
 using Moq;
 using NEL.MESH.Brokers.DateTimes;
 using NEL.MESH.Brokers.Loggings;
@@ -46,6 +48,9 @@ namespace NEL.MESH.Tests.Unit.Services.Foundations.Students
             };
         }
 
+        private static SqlException GetSqlException() =>
+            (SqlException)FormatterServices.GetUninitializedObject(typeof(SqlException));
+
         private static int GetRandomNumber() =>
             new IntRange(min: 2, max: 10).GetValue();
 
@@ -54,6 +59,9 @@ namespace NEL.MESH.Tests.Unit.Services.Foundations.Students
 
         private static DateTimeOffset GetRandomDateTimeOffset() =>
             new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Student CreateRandomStudent() =>
+            CreateStudentFiller(dateTimeOffset: GetRandomDateTimeOffset()).Create();
 
         private static Student CreateRandomStudent(DateTimeOffset dateTimeOffset) =>
             CreateStudentFiller(dateTimeOffset).Create();
---
+++ NEL.MESH/Models/Students/Exceptions/StudentDependencyException.cs
@@ -0,0 +1,11 @@
+using Xeptions;
+
+namespace NEL.MESH.Models.Students.Exceptions
+{
+    public class StudentDependencyException : Xeption
+    {
+        public StudentDependencyException(Xeption innerException) :
+            base(message: "Student dependency error occurred, contact support.", innerException)
+        { }
+    }
+}
