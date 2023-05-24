94aa2b0c6c9d82a84961c32d4b3ec0d4b1fd834e
Christo du Toit <christo@dutoit.co.uk>
2023-05-24 09:58:33 +0100

    ShouldModifyStudentAsync -> FAIL

--- NEL.MESH/Services/Foundations/Students/IStudentService.cs
+++ NEL.MESH/Services/Foundations/Students/IStudentService.cs
@@ -10,5 +10,6 @@ namespace NEL.MESH.Services.Foundations.Students
         ValueTask<Student> AddStudentAsync(Student student);
         IQueryable<Student> RetrieveAllStudents();
         ValueTask<Student> RetrieveStudentByIdAsync(Guid studentId);
+        ValueTask<Student> ModifyStudentAsync(Student student);
     }
 }
--- NEL.MESH/Services/Foundations/Students/StudentService.cs
+++ NEL.MESH/Services/Foundations/Students/StudentService.cs
@@ -47,5 +47,8 @@ namespace NEL.MESH.Services.Foundations.Students
 
                 return maybeStudent;
             });
+
+        public ValueTask<Student> ModifyStudentAsync(Student student) =>
+            throw new NotImplementedException();
     }
 }
