241e19a45d3f4d6506f89e5e75272e0966e4ddb4
Hassan Raza <h_raza110@outlook.com>
2024-01-16 11:03:47 +0000

    InvalidArgumentsMeshException

--- NEL.MESH/Models/Foundations/Mesh/Exceptions/InvalidArgumentsMeshException.cs
+++ NEL.MESH/Models/Foundations/Mesh/Exceptions/InvalidArgumentsMeshException.cs
@@ -8,9 +8,8 @@ namespace NEL.MESH.Models.Foundations.Mesh.Exceptions
 {
     public class InvalidArgumentsMeshException : Xeption
     {
-        public InvalidArgumentsMeshException()
-            : base(message:
-                "Invalid MESH argument valiation errors occurred, please correct the errors and try again.")
+        public InvalidArgumentsMeshException(string message)
+            : base(message)
         { }
     }
 }
