b2fa57acde5193479bca42e7666a1ae72906dba2
Joe Owens <joe.owens@answerdigital.com>
2025-05-21 11:51:37 +0100

    CODERUB: Reset to main

--- NEL.MESH.Tests.Acceptance/ConfigurationTests.cs
+++ NEL.MESH.Tests.Acceptance/ConfigurationTests.cs
@@ -14,7 +14,7 @@ namespace NEL.MESH.Tests.Acceptance
         {
             var configurationBuilder = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("local.appsettings.json", optional: true, reloadOnChange: true)
                 .AddEnvironmentVariables("NEL_MESH_CLIENT_ACCEPTANCE_");
 
             this.configuration = configurationBuilder.Build();
