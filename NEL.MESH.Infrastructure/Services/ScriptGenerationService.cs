589c61b0d6eea3097bb9dabdd9876b61a304c3c3
Christo du Toit <christo@dutoit.co.uk>
2026-05-13 12:44:03 +0100

    CODE RUB: Code cleanup

--- NEL.MESH.Infrastructure/Services/ScriptGenerationService.cs
+++ NEL.MESH.Infrastructure/Services/ScriptGenerationService.cs
@@ -94,6 +94,14 @@ namespace NEL.MESH.Infrastructure.Services
 
                                     Run = $"dotnet test {projectName}.Tests.Acceptance/" +
                                         $"{projectName}.Tests.Acceptance.csproj --verbosity normal"
+                                },
+
+                                new TestTask
+                                {
+                                    Name = "Integration Tests",
+
+                                    Run = $"dotnet test {projectName}.Tests.Integration/" +
+                                        $"{projectName}.Tests.Integration.csproj --verbosity normal"
                                 }
                             }
                         }
